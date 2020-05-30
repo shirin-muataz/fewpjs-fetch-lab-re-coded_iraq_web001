e464c061895b35630d0406e36763b156ba8c65cc
Shirin Muataz <shirinmuataz@gmail.com>
2020-05-30 04:49:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function fetchBooks() {
-fetch ('https://anapioficeandfire.com/api/books').then(resp => resp.json())
+return fetch ('https://anapioficeandfire.com/api/books').then(resp => resp.json())
 .then(json => renderBooks(json));
 }
 
