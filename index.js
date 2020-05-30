72e4be73034605d8c5e2e7d1075fab058f0fe455
Shirin Muataz <shirinmuataz@gmail.com>
2020-05-30 04:49:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function fetchBooks() {
-
+return fetch ('https://anapioficeandfire.com/api/books').then(resp => resp.json())
+.then(json => renderBooks(json));
 }
 
 function renderBooks(json) {
@@ -8,7 +9,7 @@ function renderBooks(json) {
     const h2 = document.createElement('h2')
     h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
-  })
+    })
 }
 
 document.addEventListener('DOMContentLoaded', function() {
